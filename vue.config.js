c3b1be81f04a7eb0e8cabd35dc25567ba1bba328
hector jose acosta manrique <hectoracosta5@gmail.com>
2022-05-25 17:23:38 -0400

    addDist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/interactive_comments/" : "/",
   transpileDependencies: true,
   chainWebpack: config => {
     config.module
