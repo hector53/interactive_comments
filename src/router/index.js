bc057f3f5297fbcd80311444e9a517bed1701c5f
hector jose acosta manrique <hectoracosta5@gmail.com>
2022-05-26 05:29:16 -0400

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/interactive_comments/'),
   routes
 })
 
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/interactive_comments/'
+}
